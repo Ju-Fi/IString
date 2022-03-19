8e7df56f58e894a4f0725a307685e125ea94d33e
Jurassic-Fighter <a.niyazov24@qhss.org>
2022-03-19 14:09:23 -0400

    redid error system

--- README.md
+++ README.md
@@ -21,12 +21,6 @@ any issues from multiple definitions), the macro must be defined in a SINGLE sou
 
 ## Notes
 
-- The `istr` struct members (`s` and `len`) are currently constant to prevent unintended modification and uses. This
-  presents an issue in there is no way to modify/reassign the value of an istring, and thus a new one must be created
-  for such uses (i.e. istr_cat). While having constant values is a good safety mechanism, if it impedes the usability of
-  this library, it will be altered so feedback is appreciated.
-- The error handling currently implemented in IString (which is only to prevent improper allocations at the moment) is
-  is not final. I am currently unsure of how I'm going to handle them going forward, so this is just a stand-in.
 - Currently the library is implemented as a single header library, meaning all you need to do to use it is include the
   header in your project and define the implementation macro in a single file. This is done for ease of packaging and
   integration; if any issues or grievances arise from this, please let me know. 
--- istr.h
+++ istr.h
@@ -1,11 +1,12 @@
 #pragma once
 
-#include <stdlib.h>
-#include <string.h>
+#include <assert.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
 typedef struct istr {
     const char *s;
@@ -14,38 +15,47 @@ typedef struct istr {
 
 typedef struct istr_builder istr_builder_t;
 
+enum istr_error_code {
+    ISTR_ERROR_ALLOC
+};
+
 istr cstr(const char *cstring);
 
 istr_builder_t istr_builder_create();
 
 istr istr_builder_convert(istr_builder_t *strbuild);
 
-#define istr_cat(a, b) _Generic((a),        \
-    istr*: _istr_cat_istr,            \
-    istr_builder_t*: _istr_cat_builder    \
+#define istr_cat(a, b) _Generic((a),		\
+	istr *: _istr_cat_istr,			\
+	istr_builder_t *: _istr_cat_builder	\
 )(a, b)
+
 
 istr _istr_cat_istr(istr *a, istr *b);
 
 void _istr_cat_builder(istr_builder_t *strbuild, istr *str);
 
-uint64_t istr_index_of(istr *str, char c); //TODO: add support to get index of a string
+uint64_t istr_index_of(istr *str, char c); // TODO: add support to get index of a string
 bool istr_cmp(istr *a, istr *b);
 
 istr istr_remove(istr *str, uint64_t index);
+
 istr istr_remove_all(istr *str, char c);
 
 istr istr_slice(istr *str, int32_t amount_to_slice);
 
-#define istr_free(a) _Generic((a),        \
-    istr*: _istr_free,            \
-    istr_builder_t*: _istr_builder_free    \
+#define istr_free(a) _Generic((a),		\
+	istr * : _istr_free,			\
+	istr_builder_t * : _istr_builder_free	\
 )(a)
 
 void _istr_free(istr *str);
 
 void _istr_builder_free(istr_builder_t *strbuild);
 
+static void istr_handle_error(enum istr_error_code code);
+
+#define ISTR_IMPLEMENT
 #ifdef ISTR_IMPLEMENT
 
 typedef struct istr_builder {
@@ -54,139 +64,158 @@ typedef struct istr_builder {
 } istr_builder_t;
 
 istr cstr(const char *cstring) {
-    istr res = {.len = strlen(cstring)};
-    res.s = malloc(sizeof(char) * (res.len + 1));
-    if (res.s == NULL) {
-        puts("Error allocating string");
-        exit(EXIT_FAILURE);
-    }
+        istr res = {.len = strlen(cstring)};
+        res.s = malloc(sizeof(char) * (res.len + 1));
+        if (res.s == NULL) {
+                istr_handle_error(ISTR_ERROR_ALLOC);
+                return res;
+        }
 
-    memcpy((void *) res.s, cstring, res.len + 1);
+        memcpy((void *) res.s, cstring, res.len + 1);
 
-    return res;
+        return res;
 }
 
 istr_builder_t istr_builder_create() {
-    istr_builder_t strbuild = {.len = 0};
-    return strbuild;
+        istr_builder_t strbuild = {.len = 0};
+        return strbuild;
 }
 
 istr istr_builder_convert(istr_builder_t *strbuild) {
-    istr res = {.s = malloc(sizeof(char) * (strbuild->len + 1)), .len = strbuild->len};
-    if (res.s == NULL) {
-        puts("Error allocating string");
-        exit(EXIT_FAILURE);
-    }
+        istr res = {.s = malloc(sizeof(char) * (strbuild->len + 1)),
+                .len = strbuild->len};
+        if (res.s == NULL) {
+                istr_handle_error(ISTR_ERROR_ALLOC);
+                return res;
+        }
 
-    memcpy((void *) res.s, strbuild->sb, res.len + 1);
+        memcpy((void *) res.s, strbuild->sb, res.len + 1);
 
-    return res;
+        return res;
 }
 
 istr _istr_cat_istr(istr *a, istr *b) {
-    istr res = {.len = a->len + b->len};
-    res.s = malloc(sizeof(char) * (res.len + 1));
-    if (res.s == NULL) {
-        puts("Error allocating string");
-        exit(EXIT_FAILURE);
-    }
+        istr res = {.len = a->len + b->len};
+        res.s = malloc(sizeof(char) * (res.len + 1));
+        if (res.s == NULL) {
+                istr_handle_error(ISTR_ERROR_ALLOC);
+                return res;
+        }
 
-    memcpy((void *) res.s, a->s, a->len);
-    memcpy((void *) (res.s + a->len), b->s, b->len + 1);
+        memcpy((void *) res.s, a->s, a->len);
+        memcpy((void *) (res.s + a->len), b->s, b->len + 1);
 
-    return res;
+        return res;
 }
 
 void _istr_cat_builder(istr_builder_t *strbuild, istr *str) {
-    bool start_of_cat = strbuild->len == 0;
-    strbuild->len += str->len;
-    if (strbuild->sb == NULL) {
-        strbuild->sb = malloc(sizeof(char) * (strbuild->len + 1));
+        bool start_of_cat = strbuild->len == 0;
+        strbuild->len += str->len;
         if (strbuild->sb == NULL) {
-            puts("Error allocating string builder");
-            exit(EXIT_FAILURE);
+                strbuild->sb = malloc(sizeof(char) * (strbuild->len + 1));
+                if (strbuild->sb == NULL) {
+                        istr_handle_error(ISTR_ERROR_ALLOC);
+                        return;
+                }
+        } else
+                strbuild->sb = realloc(strbuild->sb, strbuild->len + 1);
+
+        if (start_of_cat)
+                memcpy((void *) strbuild->sb, str->s, str->len + 1);
+        else {
+                memcpy((void *) (strbuild->sb + (strbuild->len - str->len)), str->s,
+                       str->len + 1);
         }
-    } else
-        strbuild->sb = realloc(strbuild->sb, strbuild->len + 1);
-
-    if (start_of_cat)
-        memcpy((void *) strbuild->sb, str->s, str->len + 1);
-    else {
-        memcpy((void *) (strbuild->sb + (strbuild->len - str->len)), str->s, str->len + 1);
-    }
 }
 
-void _istr_free(istr *str) {
-    free((void *) str->s);
-}
+void _istr_free(istr *str) { free((void *) str->s); }
 
-void _istr_builder_free(istr_builder_t *strbuild) {
-    free(strbuild->sb);
-}
+void _istr_builder_free(istr_builder_t *strbuild) { free(strbuild->sb); }
 
 uint64_t istr_index_of(istr *str, char c) {
-    for (size_t i = 0; i < str->len; i++) {
-        if (str->s[i] == c) return i;
-    }
-    return -1;
+        for (size_t i = 0; i < str->len; i++) {
+                if (str->s[i] == c)
+                        return i;
+        }
+        return -1;
 }
 
 bool istr_cmp(istr *a, istr *b) {
-    if (a->len != b->len) return false;
-    for (size_t i = 0; i < a->len; i++) {
-        if (a->s[i] != b->s[i]) return false;
-    }
-    return true;
+        if (a->len != b->len)
+                return false;
+        for (size_t i = 0; i < a->len; i++) {
+                if (a->s[i] != b->s[i])
+                        return false;
+        }
+        return true;
 }
 
 istr istr_remove(istr *str, uint64_t index) {
-    istr res = {.s = malloc(sizeof(char) * str->len), .len = str->len - 1};
-    if (res.s == NULL) {
-        puts("Error allocating string");
-        exit(EXIT_FAILURE);
-    }
+        istr res = {.s = malloc(sizeof(char) * str->len), .len = str->len - 1};
+        if (res.s == NULL) {
+                istr_handle_error(ISTR_ERROR_ALLOC);
+                return res;
+        }
 
-    memcpy((void *) res.s, str->s, index);
-    memcpy((void *) &res.s[index], &str->s[index + 1], str->len - index);
-    return res;
+        memcpy((void *) res.s, str->s, index);
+        memcpy((void *) &res.s[index], &str->s[index + 1], str->len - index);
+        return res;
 }
 
 istr istr_remove_all(istr *str, char c) {
-    uint32_t char_count = 0;
-    for (uint64_t i = 0; i < str->len; i++)
-        if (str->s[i] == c) char_count++;
+        uint32_t char_count = 0;
+        for (uint64_t i = 0; i < str->len; i++)
+                if (str->s[i] == c)
+                        char_count++;
 
-    istr res = {.s = malloc(sizeof(char) * (str->len - char_count + 1)), .len = str->len - char_count};
-    if (res.s == NULL) {
-        puts("Error allocating string");
-        exit(EXIT_FAILURE);
-    }
+        istr res = {.s = malloc(sizeof(char) * (str->len - char_count + 1)),
+                .len = str->len - char_count};
+        if (res.s == NULL) {
+                istr_handle_error(ISTR_ERROR_ALLOC);
+                return res;
+        }
 
-    uint64_t j = 0;
-    for (uint64_t i = 0; i < res.len; i++) {
-        if (str->s[j] == c) j++;
-        memcpy((void *) &res.s[i], &str->s[j], 1);
-        j++;
-    }
+        uint64_t j = 0;
+        for (uint64_t i = 0; i < res.len; i++) {
+                if (str->s[j] == c)
+                        j++;
+                memcpy((void *) &res.s[i], &str->s[j], 1);
+                j++;
+        }
 
-    return res;
+        return res;
 }
 
 istr istr_slice(istr *str, int amount_to_slice) {
-	bool slice_end = amount_to_slice < 0;
-	if (slice_end) amount_to_slice *= -1;
+        bool slice_end = amount_to_slice < 0;
+        if (slice_end)
+                amount_to_slice *= -1;
 
-	assert(str->len > (size_t) amount_to_slice);
+        assert(str->len > (size_t) amount_to_slice);
 
-	istr res = {.s = malloc(sizeof(char) * (str->len - amount_to_slice + 1)), .len = str->len - amount_to_slice};
-	if (res.s == NULL) {
-	        puts("Error allocating string");
-		exit(EXIT_FAILURE);
-	}
+        istr res = {.s = malloc(sizeof(char) * (str->len - amount_to_slice + 1)),
+                .len = str->len - amount_to_slice};
+        if (res.s == NULL) {
+                istr_handle_error(ISTR_ERROR_ALLOC);
+                return res;
+        }
 
-	memcpy((void*) res.s, slice_end ? str->s : &str->s[amount_to_slice], res.len);
+        memcpy((void *) res.s, slice_end ? str->s : &str->s[amount_to_slice], res.len);
 
-	return res;
+        return res;
+}
+
+static void istr_handle_error(enum istr_error_code code) {
+#ifndef ISTR_ERROR_NO_LOG
+        switch (code) {
+        case ISTR_ERROR_ALLOC:
+                perror("Error allocating string");
+                break;
+        }
+#endif
+#ifndef ISTR_ERROR_NO_EXIT
+        exit(EXIT_FAILURE);
+#endif
 }
 
 #endif
